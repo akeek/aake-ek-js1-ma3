18f06487d9c8cd19562f7a85df61fcd907d0bf12
Åke Ek <svein.ek@stud.noroff.no>
2022-02-10 23:05:11 +0100

    MA

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// Q1
+
+// Convert this to an arrow-function
+// function getRemainder(a,b) {
+//     return a % b;
+//     }
+
+const getRemainder = (a,b) => a % b
+
+getRemainder();
+
+
+//Q2
+
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=af37392c64ff4bd9836ad26850276da3"
+const htmlContainer = document.querySelector(".newHtml")
+
+async function createHtml(){
+    const response = await fetch(url);
+    const results = await response.json();
+
+    const info = results.results
+
+    htmlContainer.innerHTML = ""
+
+    for(let i = 0; i < info.length; i++){
+        if(i === 8){
+            break;
+        }
+
+        htmlContainer.innerHTML += `<div class="resultsContainer">
+                                    <div>Name: ${info[i].name}</div> 
+                                    <div>Rating: ${info[i].rating}</div> 
+                                    <div>Number of tags: ${info[i].tags.length}</div> 
+                                    </div>`
+    }
+}
+
+
+createHtml()
